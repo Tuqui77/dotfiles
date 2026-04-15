52b68839f7a4540e6a448246b879c4f636c0cfa0
Tuqui <tomasmerino58@gmail.com>
2026-04-15 18:33:34 -0300

    Update dotfiles

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -1,10 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-
+require("config.autocmds")
 
 vim.opt.switchbuf = ""
 vim.opt.fixendofline = false
 vim.opt.title = false
+
+-- Opciones para limitar las indent guides
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 require("neotest").setup({
   adapters = {
--- private_dot_config/nvim/lua/config/keymaps.lua
+++ private_dot_config/nvim/lua/config/keymaps.lua
@@ -5,10 +5,11 @@
 -- Keymaps del LSP
 vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename" })
 vim.keymap.set("n", "<leader>rd", vim.lsp.buf.definition, { desc = "Definición" })
-vim.keymap.set("n", "<leader>rr", vim.lsp.buf.references, { desc = "Referencias" })
+vim.keymap.set("n", "<leader>rr", require('telescope.builtin').lsp_references, { desc = "Referencias" })
 vim.keymap.set("n", "<leader>ri", require('telescope.builtin').lsp_implementations, { desc = "Implementación" })
 vim.keymap.set("n", "<leader>rh", vim.lsp.buf.hover, { desc = "Menú contextual" })
 vim.keymap.set("n", "<leader>rs", vim.lsp.buf.signature_help, { desc = "Signature" })
+vim.keymap.set("n", "<leader>ra", vim.lsp.buf.code_action, { desc = "Code Actions" })
 
 -- Comandos para Telescope
 local builtin = require("telescope.builtin")
@@ -37,6 +38,16 @@ end, { desc = "Find files including .git" })
 vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope live grep" })
 vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope buffers" })
 vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope help tags" })
+vim.keymap.set('n', "<leader>ft", function()
+    require('telescope.builtin').grep_string({ search = "TODO|FIXME|HACK", use_regex = true })
+end, { desc = "Search TODOs" })
 
+-- Comandos para centrar la vista al desplazar
 vim.keymap.set("i", "<C-u>", "<C-u>zz")
 vim.keymap.set("i", "<C-d>", "<C-d>zz")
+
+--Comandos para redimensionar las vistas
+vim.keymap.set('n', '<A-Up>', ':resize -2<CR>', { silent = true })
+vim.keymap.set('n', '<A-Down>', ':resize +2<CR>', { silent = true })
+vim.keymap.set('n', '<A-Left>', ':vertical resize -2<CR>', { silent = true })
+vim.keymap.set('n', '<A-Right>', ':vertical resize +2<CR>', { silent = true })
